ad32dfc6ccb48f7c8b8ec03e5152e9f527e7b333
mountainfirefly <pankajdcoder@gmail.com>
2021-08-30 08:16:14 +0530

    Change button name

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn About React
+          Learn React
         </a>
       </header>
     </div>
